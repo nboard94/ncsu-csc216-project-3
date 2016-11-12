14a7764a3afdfbc186a566f20974ac6b0dac5364
NBoar <NBoar@DESKTOP-U79JFG9>
2016-11-12 15:25:07 -0500

    Added packages in the test folder, created skeletons for classes in util

---
+++ Project3/src/edu/ncsu/csc216/todolist/util/LinkedList.java
@@ -0,0 +1,75 @@
+package edu.ncsu.csc216.todolist.util;
+
+public class LinkedList  implements List{
+
+	private static final long serialVersionUID = 0;
+	private Node head;
+	
+	public LinkedList() {
+		
+	}
+	
+	@Override
+	public boolean add(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public void add(int index, Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private Node insertAt(int i, Object o, Node n) {
+		return null;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private int indexOf(Object o, Node n, int i) {
+		return 0;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private Node remove(int i, Node n) {
+		return null;
+	}
+	
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	private int size(Node n) {
+		return 0;
+	}
+}
---
+++ Project3/src/edu/ncsu/csc216/todolist/util/Node.java
@@ -0,0 +1,12 @@
+package edu.ncsu.csc216.todolist.util;
+
+public class Node {
+
+	private static final long serialVersionUID = 0;
+	Object value;
+	private Node next;
+	
+	public Node (Object o, Node n) {
+		
+	}
+}
