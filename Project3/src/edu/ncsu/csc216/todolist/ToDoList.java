50c0df2edaf91b1f53cdcb25f7b3f7908efd8aa8
ckbyrnes <ckbyrnes@ncsu.edu>
2016-11-15 18:35:17 -0500

    Project 3: Wrote javadoc for ToDoList, generated javadoc

--- Project3/src/edu/ncsu/csc216/todolist/ToDoList.java
+++ Project3/src/edu/ncsu/csc216/todolist/ToDoList.java
@@ -17,73 +17,116 @@ import edu.ncsu.csc216.todolist.model.TaskList;
  */
 public class ToDoList extends Observable implements Serializable, Observer {
 	
-	
+	// ===== VARIABLES =====
+	/** The todo list's serial ID */
 	private static final long serialVersionUID = 34992L;
-	
-	
-	/**
-	 * Increment for increasing the capacity of the array of TaskLists
-	 */
+	/** Increment for increasing the capacity of the array of TaskLists */
 	private static final int RESIZE = 3;
-	
-	
+	/** The array of TaskLists in this todo list */
 	private TaskList[] tasks;
-	
+	/** The number of lists inside this todo list */
 	private int numLists;
-	
+	/** The list of categories in this list */
 	private CategoryList categories;
-	
+	/** The name of the file this todo list is saved to */
 	private String filename;
-	
+	/** Says if the todo list has been changed or not */
 	private boolean changed;
-	
+	/** The number ID for the next task to be added to this list */
 	private int nextTaskListNum;
 	
+	/**
+	 * Constructs a new ToDoList
+	 */
 	public ToDoList() {
 		
 	}
 	
+	/**
+	 * Returns if the todo list has been changed or not
+	 * @return The current changed value.
+	 */
 	public boolean isChanged() {
 		return false;
 	}
 	
+	/**
+	 * Sets the 'changed' variable to the given parameter
+	 * @param b the value to set 'changed' to
+	 */
 	public void setChanged(boolean b) {
 		
 	}
 	
+	/**
+	 * Gets this todo list's file name
+	 * @return The name of the file for this list
+	 */
 	public String getFilename() {
 		return null;
 	}
 	
+	/**
+	 * Sets the file name for this list to the given parameter
+	 * @param s The new file name
+	 */
 	public void setFilename(String s) {
 		
 	}
 	
+	/**
+	 * Gets the next task list number
+	 * @return The next number in the task list
+	 */
 	private int getNextTaskListNum() {
 		return 0;
 	}
 	
+	/**
+	 * Increments the task list number
+	 */
 	private void incNextTaskListNum() {
 		
 	}
 	
+	/**
+	 * Gets the number of task lists in this todo list
+	 * @return The total number of task lists inside of this todo list
+	 */
 	public int getNumTaskLists() {
 		return 0;
 	}
 	
-	public TaskList getTaskList(int i) {
+	/**
+	 * Gets the task list at the given index inside this todo list
+	 * @param idx The index of the task list to retrieve
+	 * @return The task list at the given index
+	 */
+	public TaskList getTaskList(int idx) {
 		return null;
 	}
 	
+	/**
+	 * Gets the list of categories inside this todo list
+	 * @return The list of categories as a CategoryList
+	 */
 	public CategoryList getCategoryList() {
 		return null;
 	}
 	
+	/**
+	 * TODO: Find out how this adds new task lists?
+	 * @return the index of the newly added tasklist
+	 */
 	public int addTaskList() {
 		return 0;
 	}
 	
-	public void removeTaskList(int i) {
+	/**
+	 * Removes a tasklist from this todo list given it's index in the array
+	 * @param idx the index of the tasklist to remove
+	 */
+	public void removeTaskList(int idx) {
 		
 	}
 	
