32739906b1b66ff31e17f4d5c74f1802da6ff635
NBoar <NBoar@DESKTOP-U79JFG9>
2016-11-15 13:17:23 -0500

    Implemented ArrayList and ArrayListTest

--- Project3/src/edu/ncsu/csc216/todolist/util/ArrayList.java
+++ Project3/src/edu/ncsu/csc216/todolist/util/ArrayList.java
@@ -2,66 +2,230 @@ package edu.ncsu.csc216.todolist.util;
 
 import java.io.Serializable;
 
+/**
+ * A custom implementation of the ArrayList.  Featuring the
+ * standard methods implemented from List.
+ * @author Nicholas Board and Christian Byrnes
+ */
 public class ArrayList implements List, Serializable {
 	
+	/** TheArrayList's serial identification. */
 	private static final long serialVersionUID = 28592L;
-	private static final int RESIZE = 0;
+	/** TheArrayList's RESIZE value. */
+	private static final int RESIZE = 2;
+	/** TheArrayList's internal list. */
 	private Object[] list;
+	/** TheArrayList's size. */
 	private int size;
 	
+	/**
+	 * Standard constructor for the ArrayList.
+	 * Sets the list's initial size to 1.
+	 */
 	public ArrayList() {
-		
+		list = new Object[2];
 	}
 	
-	public ArrayList(int i) {
+	/**
+	 * @param i
+	 */
+	public ArrayList(int i) throws IllegalArgumentException {
+		if (i < 1) {
+			throw new IllegalArgumentException();
+		}
 		
+		list = new Object[i];
 	}
 	
+    /**
+     * Appends the specified element to the end of this list (optional
+     * operation).
+     *
+     * Lists that support this operation may place limitations on what
+     * elements may be added to this list.  In particular, some
+     * lists will refuse to add null elements, and others will impose
+     * restrictions on the type of elements that may be added.  List
+     * classes should clearly specify in their documentation any restrictions
+     * on what elements may be added.
+     *
+     * @param o element to be appended to this list
+     * @return true (as specified by {@link Collection#add})
+     */
 	@Override
 	public boolean add(Object o) {
-		// TODO Auto-generated method stub
-		return false;
+		if (size() == list.length) {
+			resize();
+		}
+		
+		list[size()] = o;
+		size++;
+		return true;
 	}
 	
+    /**
+     * Inserts the specified element at the specified position in this list
+     * (optional operation).  Shifts the element currently at that position
+     * (if any) and any subsequent elements to the right (adds one to their
+     * indices).
+     *
+     * @param index index at which the specified element is to be inserted
+     * @param element element to be inserted
+     * @throws NullPointerException if the specified element is null and
+     *         this list does not permit null elements
+     * @throws IllegalArgumentException if some property of the specified
+     *         element prevents it from being added to this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *         (index < 0 || index > size())
+     */
 	@Override
-	public void add(int index, Object element) {
-		// TODO Auto-generated method stub
+	public void add(int index, Object element) throws NullPointerException, IllegalArgumentException, IndexOutOfBoundsException {
+		if (element == null) {
+			throw new NullPointerException();
+		}
 		
+//TODO
+//		if (something) {
+//			throw new IllegalArgumentException();
+//		}
+			
+		if (index < 0 || index > size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		if (size() == list.length) {
+			resize();
+		}
+		
+		for (int i = size(); i > index; i--) {
+			this.list[i] = this.list[i-1];
+		}
+		
+		this.list[index] = element;
+		size++;
 	}
 
+	/**
+     * Returns true if this list contains the specified element.
+     * More formally, returns true if and only if this list contains
+     * at least one element e such that
+     * (o==null ? e==null : o.equals(e)).
+     *
+     * @param o element whose presence in this list is to be tested
+     * @return true if this list contains the specified element
+     */
 	@Override
 	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < size(); i++) {
+			if (this.list[i].equals(o)) {
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
+    /**
+     * Returns the element at the specified position in this list.
+     *
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *         (index < 0 || index >= size())
+     */
 	@Override
-	public Object get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object get(int index) throws IndexOutOfBoundsException {
+		if (index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		return this.list[index];
 	}
 
+	/**
+     * Returns the index of the first occurrence of the specified element
+     * in this list, or -1 if this list does not contain the element.
+     * More formally, returns the lowest index i such that
+     * (o==null ? get(i)==null : o.equals(get(i))),
+     * or -1 if there is no such index.
+     *
+     * @param o element to search for
+     * @return the index of the first occurrence of the specified element in
+     *         this list, or -1 if this list does not contain the element
+     */
 	@Override
 	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		for (int i = 0; i < size; i++) {
+			if (this.list[i].equals(o)) {
+				return i;
+			}
+		}
+		
+		return -1;
 	}
 
+	/**
+     * Returns true if this list contains no elements.
+     *
+     * @return true if this list contains no elements
+     */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		if (size > 0) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
+	 /**
+     * Removes the element at the specified position in this list (optional
+     * operation).  Shifts any subsequent elements to the left (subtracts one
+     * from their indices).  Returns the element that was removed from the
+     * list.
+     *
+     * @param index the index of the element to be removed
+     * @return the element previously at the specified position
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *         (index < 0 || index >= size())
+     */
 	@Override
-	public Object remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object remove(int index) throws IndexOutOfBoundsException {
+		if (index < 0 || index >= size()) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		Object removed = this.list[index];
+		for (int i = index; i < this.list.length - 1; i++) {
+			this.list[i] = this.list[i + 1];
+		}
+		
+		size--;
+		return removed;
+		
 	}
 	
+    /**
+     * Returns the number of elements in this list.  If this list contains
+     * more than Integer.MAX_VALUE elements, returns
+     * Integer.MAX_VALUE.
+     *
+     * @return the number of elements in this list
+     */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
+	}
+	
+	/**
+	 * Resizes the Array if it is too small.
+	 */
+	private void resize() {
+		Object[] newList = new Object[size()*RESIZE];
+		
+		for (int i = 0; i < size(); i++) {
+			newList[i] = list[i];
+		}
+		
+		list = newList;
 	}
 }
--- Project3/test/edu/ncsu/csc216/todolist/util/ArrayListTest.java
+++ Project3/test/edu/ncsu/csc216/todolist/util/ArrayListTest.java
@@ -1,5 +1,185 @@
 package edu.ncsu.csc216.todolist.util;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class ArrayListTest {
 
+	@Test
+	public void testArrayList() {
+		ArrayList l1 = new ArrayList();
+		assertTrue(l1.isEmpty());
+		
+		ArrayList l2 = null;
+		try {
+			l2 = new ArrayList(0);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertNull(l2);
+		}
+		
+		try {
+			l2 = new ArrayList(3);
+			assertTrue(l2.isEmpty());
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testAdd() {
+		ArrayList l1 = new ArrayList();
+		
+		l1.add("Avagantamous");
+		assertFalse(l1.isEmpty());
+		assertEquals(1, l1.size());
+		assertEquals("Avagantamous", l1.get(0));
+		
+		l1.add("Hydran");
+		assertFalse(l1.isEmpty());
+		assertEquals(2, l1.size());
+		assertEquals("Hydran", l1.get(1));
+		
+		l1.add("T");
+		assertFalse(l1.isEmpty());
+		assertEquals(3, l1.size());
+		assertEquals("T", l1.get(2));
+	
+	}
+	
+	@Test
+	public void testAddIndex() {
+		ArrayList l1 = new ArrayList();
+		
+		try {
+			l1.add(0, null);
+			fail();
+		} catch (NullPointerException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		try {
+			l1.add(-1, "Rox");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		try {
+			l1.add(1, "Rox");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		l1.add(0, "Avagantamous");
+		assertFalse(l1.isEmpty());
+		assertEquals(1, l1.size());
+		assertEquals("Avagantamous", l1.get(0));
+		
+		l1.add(0, "Hydran");
+		assertFalse(l1.isEmpty());
+		assertEquals(2, l1.size());
+		assertEquals("Hydran", l1.get(0));
+		assertEquals("Avagantamous", l1.get(1));
+		
+		l1.add(2, "T");
+		assertFalse(l1.isEmpty());
+		assertEquals(3, l1.size());
+		assertEquals("T", l1.get(2));
+		assertEquals("Hydran", l1.get(0));
+		assertEquals("Avagantamous", l1.get(1));
+		
+		l1.add(1, "Rabi");
+		assertFalse(l1.isEmpty());
+		assertEquals(4, l1.size());
+		assertEquals("T", l1.get(3));
+		assertEquals("Hydran", l1.get(0));
+		assertEquals("Avagantamous", l1.get(2));
+		assertEquals("Rabi", l1.get(1));
+	}
+	
+	@Test
+	public void testContains() {
+		ArrayList l1 = new ArrayList();
+
+		l1.add("Hydran");
+		assertTrue(l1.contains("Hydran"));
+		
+		l1.add("T");
+		assertTrue(l1.contains("T"));
+		
+		assertFalse(l1.contains("Rabi"));
+		assertFalse(l1.contains("Avagantamous"));
+	}
+	
+	@Test
+	public void testGet() {
+		ArrayList l1 = new ArrayList();
+		l1.add("Hydran");	
+		l1.add("T");
+		
+		try {
+			l1.get(-1);
+			fail();
+		} catch(IndexOutOfBoundsException e) {
+			assertEquals("Hydran", l1.get(0));
+		}
+		
+		try {
+			l1.get(3);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertEquals("T", l1.get(1));
+		}
+	}
+	
+	@Test
+	public void testIndexOf() {
+		ArrayList l1 = new ArrayList();
+		l1.add("Hydran");	
+		l1.add("T");
+		
+		assertEquals(0, l1.indexOf("Hydran"));
+		assertEquals(1, l1.indexOf("T"));
+		
+		assertEquals(-1, l1.indexOf("Avagantamous"));
+	}
+	
+	@Test
+	public void testRemove() {
+		ArrayList l1 = new ArrayList();
+		
+		try {
+			l1.remove(-1);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		try {
+			l1.remove(0);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		try {
+			l1.remove(1);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		l1.add("Hydran");	
+		l1.add("T");
+		
+		try {
+			assertEquals("Hydran", l1.remove(0));
+			assertEquals("T", l1.get(0));
+		} catch (IndexOutOfBoundsException e) {
+			fail();
+		}
+	}
 }
