451a99194c8093a924e754c800ce4df4e5b2db94
NBoar <NBoar@DESKTOP-U79JFG9>
2016-11-15 19:09:15 -0500

    Javadoc in TaskTableModel based off of CategoryTableModel

--- Project3/src/edu/ncsu/csc216/todolist/ui/TaskTableModel.java
+++ Project3/src/edu/ncsu/csc216/todolist/ui/TaskTableModel.java
@@ -2,41 +2,89 @@ package edu.ncsu.csc216.todolist.ui;
 
 import java.io.Serializable;
 
+/**
+ * TaskTableModel is a wrapper for the information in TaskList that 
+ * can be used by a JTable. 
+ * @author Nicholas Board and Christian Byrnes
+ */
 public class TaskTableModel implements Serializable {
 
+	/** Serial version UID */
 	private static final long serialVersionUID = 5954551753060998701L;
+	/** Names for each of the columns in the TableModel */
 	private String[] colNames;
+	/** Array of CategoryList information */
 	private Object[][] data;
 	
-	public TaskTableModel(Object[][] o) {
+	/**
+	 * Creates the model from the given data.
+	 * @param data the data to populate the TableModel
+	 */
+	public TaskTableModel(Object[][] data) {
 		
 	}
 	
+	/**
+	 * Returns the number of rows in the data.
+	 * @return the number of rows in the data
+	 */
 	public int getRowCount() {
 		return 0;
 	}
 	
+	/**
+	 * Returns the number of columns in the data.
+	 * @return the number of columns in the data 
+	 */
 	public int getColumnCount() {
 		return 0;
 	}
 	
-	public String getColumnName(int i) {
+	/**
+	 * Returns the column name at the given index.
+	 * @param col the index for finding the column name
+	 * @return the column name at the given index
+	 */
+	public String getColumnName(int col) {
 		return null;
 	}
 	
-	public Object getValueAt(int i1, int i2) {
+	/**
+	 * Returns the value at the given cell in the TableModel.
+	 * @param row the index for the row
+	 * @param col the index for the column
+	 * @return the value in the data at the given row and col
+	 */
+	public Object getValueAt(int row, int col) {
 		return null;
 	}
 	
-	public void setValueAt(Object i, int i1, int i2) {
+	/**
+	 * Sets the value to the given cell in the TableModel.
+	 * @param value the value to set
+	 * @param row the index for the row
+	 * @param col the index for the column
+	 */
+	public void setValueAt(Object value, int row, int col) {
 		
 	}
 	
-	public TaskData getTaskRowData(int i) {
+	/**
+	 * Returns the TaskData object associated with the given
+	 * row in the TableModel.
+	 * @param row the TaskData to return
+	 * @return the TaskData for the given row
+	 */
+	public TaskData getTaskRowData(int row) {
 		return null;
 	}
 	
-	public void setTaskRowRata(int i, TaskData t) {
+	/**
+	 * Sets the given row with the provided TaskData.
+	 * @param d TaskyData to set in the row
+	 * @param row the row to set
+	 */
+	public void setTaskRowRata(int d, TaskData row) {
 		
 	}
 }
