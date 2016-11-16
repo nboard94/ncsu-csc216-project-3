66212a765ba687a04b498bb8c558c85e52c015e8
NBoar <NBoar@DESKTOP-U79JFG9>
2016-11-15 19:01:17 -0500

    Javadoc in the GUI classes.

--- Project3/src/edu/ncsu/csc216/todolist/ui/TaskEditPane.java
+++ Project3/src/edu/ncsu/csc216/todolist/ui/TaskEditPane.java
@@ -15,8 +15,13 @@ import javax.swing.JTextField;
 import edu.ncsu.csc216.todolist.model.Category;
 import edu.ncsu.csc216.todolist.model.CategoryList;
 
+/**
+ * Panel for editing Tasks.
+ * @author Nicholas Board and Christian Byrnes
+ */
 public class TaskEditPane extends Component implements Serializable {
 	
+	/** Serial version UID */
 	private static final long serialVersionUID = 5479139338455751629L;
 	private CategoryList categories;
 	private JTextField taskID;
@@ -31,6 +36,9 @@ public class TaskEditPane extends Component implements Serializable {
 	private boolean edit;
 	private TaskData data;
 	
+	/**
+	 * @param c
+	 */
 	public TaskEditPane(CategoryList c) {
 		
 	}
@@ -39,114 +47,219 @@ public class TaskEditPane extends Component implements Serializable {
 		
 	}
 	
+	/**
+	 * Initializes the GUI.
+	 */
 	private void init() {
 		
 	}
 	
+	/**
+	 * Initializes the view.
+	 */
 	private void initView() {
 		
 	}
 	
+	/**
+	 * Returns the spinner for TaskStart.
+	 * @return the spinner for TaskStart.
+	 */
 	JSpinner getTaskStartSpinner() {
 		return null;
 	}
 	
+	/**
+	 * Returns the spinner for TaskDue.
+	 * @return the spinner for TaskDue.
+	 */
 	JSpinner getTaskDueSpinner() {
 		return null;
 	}
 	
+	/**
+	 * Returns the spinner for TaskCompleted.
+	 * @return the spinner for TaskCompleted.
+	 */
 	JSpinner getTaskCompletedSpinner() {
 		return null;
 	}
 	
+	/**
+	 * Returns the task start date.
+	 * @return the task start date.
+	 */
 	Date getTaskStart() {
 		return null;
 	}
 	
+	/**
+	 * Returns the task due date.
+	 * @return the task due date.
+	 */
 	Date getTaskDue() {
 		return null;
 	}
 	
+	/**
+	 * Returns the task completed date.
+	 * @return the task completed date.
+	 */
 	Date getTaskCompleted() {
 		return null;
 	}
 	
+	/**
+	 * Returns the text field for taskID.
+	 * @return the text field for taskID.
+	 */
 	JTextField getTaskID() {
 		return null;
 	}
 	
+	/**
+	 * Returns the text field for taskTitle.
+	 * @return the text field for taskTitle.
+	 */
 	JTextField getTaskTitle() {
 		return null;
 	}
 	
+	/**
+	 * Returns the combo box for task's categories.
+	 * @return the combo box for task's categories.
+	 */
 	JComboBox<Category> getCategory() {
 		return null;
 	}
 	
+	/**
+	 * Returns the text area for the taskDetails.
+	 * @return the test area for the taskDetauls.
+	 */
 	JTextArea getTaskDetails() {
 		return null;
 	}
 	
+	/**
+	 * Returns the check box for task complete.
+	 * @return the check box for task complete.
+	 */
 	JCheckBox getComplete() {
 		return null;
 	}
 	
+	/**
+	 * Sets the start date for the Task.
+	 * @param d the start date for the Task.
+	 */
 	void setTaskStart(Date d) {
 		
 	}
 	
+	/**
+	 * Sets the due date for the Task.
+	 * @param d the due date for the Task.
+	 */
 	void setTaskDue(Date d) {
 		
 	}
 	
+	/**
+	 * Sets the completed date for the Task.
+	 * @param d the completed date for the Task.
+	 */
 	void setTaskCompleted(Date d) {
 		
 	}
 	
+	/**
+	 * Returns true if in add mode.
+	 * @return true if in add mode
+	 */
 	boolean isAddMode() {
 		return false;
 	}
 	
+	/**
+	 * Returns true if in edit mode.
+	 * @return true if in edit mode.
+	 */
 	boolean isEditMode() {
 		return false;
 	}
 	
+	/**
+	 * Enables add mode and disables edit.
+	 */
 	void enableAdd() {
 		
 	}
 	
+	/**
+	 * Disables add mode.
+	 */
 	void disableAdd() {
 		
 	}
 	
+	/**
+	 * Enables edit mode and disables add.
+	 * @param t TaskData to populate the edit area with.
+	 */
 	void enableEdit(TaskData t) {
 		
 	}
 	
+	/**
+	 * Disables edit mode.
+	 */
 	void disableEdit() {
 		
 	}
 	
+	/**
+	 * Returns true if the required fields are not empty.
+	 * @return true if the required fields are not empty.
+	 */
 	boolean fieldsNotEmpty() {
 		return false;
 	}
 	
+	/**
+	 * Initializes the TaskData to the given value.
+	 * @param t new TaskData
+	 */
 	void setTaskData(TaskData t) {
 		
 	}
 	
+	/**Adds the given EventListner to the txtTaskName and
+	 * txtTaskDescription text fields.
+	 * @param e EventListner to add to text fields.
+	 */
 	void addFieldListener(EventListener e) {
 		
 	}
 	
+	/**
+	 * Fills the fields with the appropriate text from the CategoryData field.
+	 */
 	void fillFields() {
 		
 	}
 	
+	/**
+	 * Clears the fields by setting data to null.
+	 */
 	void clearFields() {
 		
 	}
 	
+	/**
+	 * Returns the fields as a TaskData object.
+	 * @return the fields as a TaskData object.
+	 */
 	TaskData getFields() {
 		return null;
 	}
--- Project3/src/edu/ncsu/csc216/todolist/ui/TaskListPane.java
+++ Project3/src/edu/ncsu/csc216/todolist/ui/TaskListPane.java
@@ -9,35 +9,71 @@ import javax.swing.JTable;
 
 import edu.ncsu.csc216.todolist.model.TaskList;
 
+/**
+ * Maintains a list of Tasks.
+ * @author Nicholas Board and Christian Byrnes
+ */
 public class TaskListPane extends Component implements Observer, Serializable {
 
+	/** Serial version UID */
 	private static final long serialVersionUID = -2210716111020406799L;
+	/** List of tasks to display */
 	private TaskList tasks;
+	/** TaskTableModel which displays the list of tasks */
 	private TaskTableModel ttm;
+	/** Table for the tasks */
 	private JTable table;
+	/** Width of columns */
 	private int[] coldWidths;
 	
-	public TaskListPane(TaskList t) {
+	
+	/**
+	 * Creates the TaskListPane that shows the TaskData in a table.
+	 * @param taskList TaskList of Task objects.
+	 */
+	public TaskListPane(TaskList taskList) {
 		
 	}
 	
+	/**
+	 * Returns the TaskTableModel.
+	 * @return the TaskTableModel
+	 */
 	public TaskTableModel getTaskTableModel() {
 		return null;
 	}
 
+	/**
+	 * Returns the JTable
+	 * @return the JTable
+	 */
 	public JTable getTable() {
 		return null;
 	}
 	
+	/**
+	 * Initializes the view by creating the JTable (that is wrapped in JScrollPane)
+	 * and associating the JTable with the TaskTableModel.
+	 */
 	private void initView() {
 		
 	} 
 	
+	/**
+	 * Clears the selection.
+	 */
 	public void clearSelection() {
 		
 	}
 	
-	public void update(Observable o1, Object o2) {
+	/**
+	 * This method is called by the observed object, whenever the observed object
+	 * is changed.  In this case, the observed object is the TaskList. Any changes 
+	 * to the TaskList will lead to an update of the TaskTableModel.
+	 * @param o the observable object
+	 * @param arg any additional information needed about the change.
+	 */
+	public void update(Observable o, Object arg) {
 		
 	}
 }
