cc9566fd19cd92e4104e7483b10c4e75bde918ce
ckbyrnes <ckbyrnes@ncsu.edu>
2016-11-21 15:31:01 -0500

    Project 3: Implemented LinkedList
    * Implemented LinkedList, troubleshot till green bar in tests
    
    * 94.5% coverage for LinkedList through LinkedListTest
    
    * Next, implementation of TaskList by utilizing LinkedList

--- Project3/src/edu/ncsu/csc216/todolist/util/LinkedList.java
+++ Project3/src/edu/ncsu/csc216/todolist/util/LinkedList.java
@@ -18,7 +18,7 @@ public class LinkedList  implements List, Serializable {
 	 * The standard constructor for a LinkedList.
 	 */
 	public LinkedList() {
-		
+		head = new Node(null, null);
 	}
 	
     /**
@@ -34,11 +34,18 @@ public class LinkedList  implements List, Serializable {
      *
      * @param o element to be appended to this list
      * @return True if the object is successfully added to the list
+     * @throws IllegalArgumentException if the item given is null
      */
 	@Override
 	public boolean add(Object o) {
-		// TODO Auto-generated method stub
-		return false;
+		//check if the object is null, throw exception if bad
+		if (o == null) throw new IllegalArgumentException();
+		
+		//otherwise, add this to the end of the list
+		add(this.size(), o);
+		
+		return true;
+		
 	}
 	
     /**
@@ -57,8 +64,13 @@ public class LinkedList  implements List, Serializable {
      */
 	@Override
 	public void add(int index, Object element) {
-		// TODO Auto-generated method stub
+		//check if the object is null, throw exception if it is
+		if (element == null) throw new IllegalArgumentException();
+		//also, if the index is greater than size throw an index out of bounds exception
+		if (index > this.size() || index < 0) throw new IndexOutOfBoundsException();
 		
+		//otherwise, we'll throw this to the insertAt method, giving it the index, the element, and front
+		this.insertAt(index, element, head);
 	}
 	
 	/**
@@ -72,7 +84,19 @@ public class LinkedList  implements List, Serializable {
 	 * @return The newly attached node.
 	 */
 	private Node insertAt(int index, Object element, Node node) {
-		return null;
+		//base case, if index = 0
+		if (index == 0) {
+			//create the new node
+			Node newNode = new Node(element, node.next);
+			//set the given node's next to this new node
+			node.next = newNode;
+			//return the new node
+			return newNode;
+		}
+		//otherwise, we'll pass it down
+		else {
+			return this.insertAt(index - 1, element, node.next);
+		}
 	}
 
     /**
@@ -86,7 +110,18 @@ public class LinkedList  implements List, Serializable {
      */
 	@Override
 	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
+		//if the list is empty return false
+		if (this.isEmpty()) return false;
+		
+		//else, lets roll through the list
+		Node current = head.next;
+		
+		while (current != null) {
+			if (current.value.equals(o)) return true;
+			current = current.next;
+		}
+		
+		//if you're out here, the whole list didn't contain the object
 		return false;
 	}
 
@@ -99,8 +134,17 @@ public class LinkedList  implements List, Serializable {
      */
 	@Override
 	public Object get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		//if the index is illegal have a fit
+		if (index < 0 || index >= this.size()) throw new IndexOutOfBoundsException();
+		
+		//goes through the list until the item is retrieved
+		Node current = head.next;
+		
+		for (int i = 0; i < index; i ++) {
+			current = current.next;
+		}
+		
+		return current.value;
 	}
 
     /**
@@ -116,12 +160,31 @@ public class LinkedList  implements List, Serializable {
      */
 	@Override
 	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		//if the list is empty then we'll always return -1
+		if (this.isEmpty()) return -1;
+		
+		//otherwise, we're going to use a private recursive method
+		return this.indexOf(o, head.next, 0);
+		
 	}
 
-	private int indexOf(Object o, Node n, int i) {
-		return 0;
+	/**
+	 * Recursive private method used to search the list for a certain object value
+	 * @param obj the object to search for
+	 * @param node the node to check the value of
+	 * @param idx the current index
+	 * @return the index of the object, if there is one.
+	 */
+	private int indexOf(Object obj, Node node, int idx) {
+		//first check if this node contains the value we're looking for
+		if (node.value.equals(obj)) return idx;
+		
+		//if this node doesn't contain the value we're looking for and there are no more
+		//nodes left, we'll return -1
+		if (node.next == null) return -1;
+		
+		//otherwise then we're going to move forward with node.next, adding one to the index
+		return this.indexOf(obj, node.next, idx + 1);
 	}
 	
     /**
@@ -131,8 +194,8 @@ public class LinkedList  implements List, Serializable {
      */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		if (head.next == null) return true;
+		else return false;
 	}
 
     /**
@@ -147,12 +210,31 @@ public class LinkedList  implements List, Serializable {
      */
 	@Override
 	public Object remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		//check the index, have a hissy fit if it's illegal
+		if (index < 0 || index >= this.size()) throw new IndexOutOfBoundsException();
+		
+		//toss this bad boy to the R&R- recursive remove method
+		return remove(index, head).value;
 	}
 	
-	private Node remove(int i, Node n) {
-		return null;
+	/**
+	 * A recursive method that takes an index and a node- if the index given is 0, then
+	 * this node's next is the one to be removed from the list
+	 * @param idx the index of the node to be removed
+	 * @param n the node to check
+	 * @return the 
+	 */
+	private Node remove(int idx, Node n) {
+		//base case, index = 0
+		if (idx == 0) {
+			//grab the old node, then set it's next to the current node's next
+			Node oldNode = n.next;
+			n.next = oldNode.next;
+			return oldNode;
+		}
+		
+		//otherwise, lets dance down the list
+		return remove(idx - 1, n.next);
 	}
 	
     /**
@@ -164,8 +246,7 @@ public class LinkedList  implements List, Serializable {
      */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size(head);
 	}
 	
 	/**
@@ -174,7 +255,8 @@ public class LinkedList  implements List, Serializable {
 	 * @return The size of the LinkedList.
 	 */
 	private int size(Node n) {
-		return 0;
+		if (n.next == null) return 0;
+		else return size(n.next) + 1;
 	}
 	
 	/**
--- Project3/test/edu/ncsu/csc216/todolist/util/LinkedListTest.java
+++ Project3/test/edu/ncsu/csc216/todolist/util/LinkedListTest.java
@@ -1,9 +1,203 @@
 package edu.ncsu.csc216.todolist.util;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 /**
  * Class to test LinkedList class and it's methods.
  * @author Nicholas Board and Christian Byrnes
  */
 public class LinkedListTest {
 
+	/**
+	 * Tests LinkedList's constructor.
+	 */
+	@Test
+	public void testLinkedList() {
+		LinkedList l1 = new LinkedList();
+		assertTrue(l1.isEmpty());
+		
+	}
+	
+	/**
+	 * Tests LinkedList's add method.
+	 */
+	@Test
+	public void testAdd() {
+		LinkedList l1 = new LinkedList();
+		
+		l1.add("Avagantamous");
+		assertFalse(l1.isEmpty());
+		assertEquals(1, l1.size());
+		assertEquals("Avagantamous", l1.get(0));
+		
+		l1.add("Hydran");
+		assertFalse(l1.isEmpty());
+		assertEquals(2, l1.size());
+		assertEquals("Hydran", l1.get(1));
+		
+		l1.add("T");
+		assertFalse(l1.isEmpty());
+		assertEquals(3, l1.size());
+		assertEquals("T", l1.get(2));
+	
+	}
+	
+	/**
+	 * Tests LinkedList's add method with int parameter.
+	 */
+	@Test
+	public void testAddIndex() {
+		LinkedList l1 = new LinkedList();
+		
+		
+		try {
+			l1.add(null);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(l1.isEmpty());
+		}
+		try {
+			l1.add(0, null);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		try {
+			l1.add(-1, "Rox");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		try {
+			l1.add(1, "Rox");
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		l1.add(0, "Avagantamous");
+		assertFalse(l1.isEmpty());
+		assertEquals(1, l1.size());
+		assertEquals("Avagantamous", l1.get(0));
+		
+		l1.add(0, "Hydran");
+		assertFalse(l1.isEmpty());
+		assertEquals(2, l1.size());
+		assertEquals("Hydran", l1.get(0));
+		assertEquals("Avagantamous", l1.get(1));
+		
+		l1.add(2, "T");
+		assertFalse(l1.isEmpty());
+		assertEquals(3, l1.size());
+		assertEquals("T", l1.get(2));
+		assertEquals("Hydran", l1.get(0));
+		assertEquals("Avagantamous", l1.get(1));
+		
+		l1.add(1, "Rabi");
+		assertFalse(l1.isEmpty());
+		assertEquals(4, l1.size());
+		assertEquals("T", l1.get(3));
+		assertEquals("Hydran", l1.get(0));
+		assertEquals("Avagantamous", l1.get(2));
+		assertEquals("Rabi", l1.get(1));
+	}
+	
+	/**
+	 * Tests LinkedList's contains method.
+	 */
+	@Test
+	public void testContains() {
+		LinkedList l1 = new LinkedList();
+
+		l1.add("Hydran");
+		assertTrue(l1.contains("Hydran"));
+		
+		l1.add("T");
+		assertTrue(l1.contains("T"));
+		
+		assertFalse(l1.contains("Rabi"));
+		assertFalse(l1.contains("Avagantamous"));
+	}
+	
+	/**
+	 * Tests LinkedList's get method.
+	 */
+	@Test
+	public void testGet() {
+		LinkedList l1 = new LinkedList();
+		l1.add("Hydran");	
+		l1.add("T");
+		
+		try {
+			l1.get(-1);
+			fail();
+		} catch(IndexOutOfBoundsException e) {
+			assertEquals("Hydran", l1.get(0));
+		}
+		
+		try {
+			l1.get(3);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertEquals("T", l1.get(1));
+		}
+	}
+	
+	/**
+	 * Tests LinkedList's indexOf method.
+	 */
+	@Test
+	public void testIndexOf() {
+		LinkedList l1 = new LinkedList();
+		l1.add("Hydran");	
+		l1.add("T");
+		
+		assertEquals(0, l1.indexOf("Hydran"));
+		assertEquals(1, l1.indexOf("T"));
+		
+		assertEquals(-1, l1.indexOf("Avagantamous"));
+	}
+	
+	/**
+	 * Tests LinkedList's remove method.
+	 */
+	@Test
+	public void testRemove() {
+		LinkedList l1 = new LinkedList();
+		
+		try {
+			l1.remove(-1);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		try {
+			l1.remove(0);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		try {
+			l1.remove(1);
+			fail();
+		} catch (IndexOutOfBoundsException e) {
+			assertTrue(l1.isEmpty());
+		}
+		
+		l1.add("Hydran");	
+		l1.add("T");
+		
+		try {
+			assertEquals("Hydran", l1.remove(0));
+			assertEquals("T", l1.get(0));
+		} catch (IndexOutOfBoundsException e) {
+			fail();
+		}
+	}
 }
