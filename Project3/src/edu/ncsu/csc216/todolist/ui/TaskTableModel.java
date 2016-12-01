4c24a8af2cacedefd210b9b93498d9f67c157009
marsm_000 <marsm_000@David>
2016-11-30 19:18:17 -0500

    Remove unnecesary TODOs

--- Project3/src/edu/ncsu/csc216/todolist/ui/TaskTableModel.java
+++ Project3/src/edu/ncsu/csc216/todolist/ui/TaskTableModel.java
@@ -96,25 +96,25 @@ public class TaskTableModel extends AbstractTableModel implements Serializable, 
 
 	@Override
 	public void addTableModelListener(TableModelListener arg0) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public Class<?> getColumnClass(int arg0) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public boolean isCellEditable(int arg0, int arg1) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public void removeTableModelListener(TableModelListener arg0) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 }
