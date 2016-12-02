5a91f6b5af2cb4b9f691e5f4ee55f01c7b7670c0
marsm_000 <marsm_000@David>
2016-12-02 09:39:42 -0500

    update update() todolist

--- Project3/src/edu/ncsu/csc216/todolist/ToDoList.java
+++ Project3/src/edu/ncsu/csc216/todolist/ToDoList.java
@@ -269,14 +269,14 @@ public class ToDoList extends Observable implements Serializable, Observer {
 	 * @param arg the arguments to pass up to notifyObservers()
 	 */
 	public void update(Observable o, Object arg) {
-		if (o.equals(categories)) {
+		if (o.getClass().equals(categories.getClass())) {
 			this.setChanged();
 			this.notifyObservers();
 			changed = true;
 		}
 		else {
 			for (int i = 0; i < tasks.length; i++) {
-				if (o.equals(tasks[i])) {
+				if (o.getClass().equals(tasks[i].getClass())) {
 					this.setChanged();
 					this.notifyObservers();
 					changed = true;
