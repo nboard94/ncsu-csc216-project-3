b1a9764813e337e24fc8ebac9cfdd73dbb63fa78
marsm_000 <marsm_000@David>
2016-12-02 11:40:34 -0500

    Test addTask solution

--- Project3/src/edu/ncsu/csc216/todolist/model/TaskList.java
+++ Project3/src/edu/ncsu/csc216/todolist/model/TaskList.java
@@ -138,7 +138,7 @@ public class TaskList extends Observable implements Observer, Tabular, Serializa
 		while (!spotFound) {
 			
 			//if the spotIdx is equal to size, we'll be adding the task to the end of the list
-			if (spotIdx == list.size() - 1) {
+			if (spotIdx == list.size()) {
 				list.add(newTask);
 				newTask.addObserver(this);
 				this.incNextTaskNum();
